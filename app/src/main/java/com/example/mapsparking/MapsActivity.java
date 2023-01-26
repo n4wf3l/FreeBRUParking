539b157ddc03116f5b371c7a33086956219e10fd
n4wf3l <nawfel.ajari@student.ehb.be>
2023-01-26 15:09:13 +0100

    Yellow CurrentLocation Marker

--- app/src/main/java/com/example/mapsparking/MapsActivity.java
+++ app/src/main/java/com/example/mapsparking/MapsActivity.java
@@ -98,15 +98,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -226,7 +218,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if (location != null) {
                     currentLocation = location;
                     LatLng userLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-                    mMap.addMarker(new MarkerOptions().position(userLatLng).title("Your Location"));
+                    mMap.addMarker(new MarkerOptions().position(userLatLng).title("Your Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
                     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 15));
                 }
             }
