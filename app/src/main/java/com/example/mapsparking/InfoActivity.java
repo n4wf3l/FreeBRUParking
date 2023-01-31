d7b8e5f790527811830ccc1b49039170a6e89075
n4wf3l <nawfel.ajari@student.ehb.be>
2023-01-31 20:21:44 +0100

    LastModifications

---
+++ app/src/main/java/com/example/mapsparking/InfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.mapsparking;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        //Views
+    }
+}
